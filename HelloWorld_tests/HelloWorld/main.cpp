655ebe30e3d013190991307ef75481442e5ff45c
Miroslava Novikova <diwaina2@yandex.ru>
2025-08-31 02:34:58 +0700

    Фикс окна

--- HelloWorld_tests/HelloWorld/main.cpp
+++ HelloWorld_tests/HelloWorld/main.cpp
@@ -5,5 +5,9 @@
 int main() {
     std::cout << sayHello() << std::endl;
     std::cout << "Version: " << test::getVersion() << std::endl;
+
+    std::cout << "Press ENTER to continue...";
+    std::cin.get();
+
     return 0;
 }
